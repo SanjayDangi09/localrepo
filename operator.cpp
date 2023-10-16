07c58626667aaa9fb138dc65c8975acc24d8d077
sanjaydangi09 <sanjaydangi9358@gmail.com>
2023-10-16 21:15:38 +0530

    Add  a operator program

---
+++ operator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+
+    int a=4;
+    int b=6;
+
+
+    cout<<"a&b:" <<(a&b)<<endl;
+    cout<<"a|b:"<< (a|b)<<endl;
+    cout<<"a~b:"<< ~a<<endl;
+    cout<<"a^b:" <<(a^b)<<endl;
+
+
+    // left shift and right shift
+
+    cout<<"a<<b"<<(19<<2)<<endl;
+    cout<<"a>>b"<<(17>>2)<<endl;
+
+}
